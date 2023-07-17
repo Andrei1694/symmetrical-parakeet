8c3cae50b6ead973d07fc631e1b7423e6fcb27a4
Andrei1694 <andrei4.stanciu@gmail.com>
2023-07-17 14:28:25 +0300

    fixed swiper height

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -70,6 +70,9 @@ export default function HomePage() {
               slidesPerView={1}
               onSlideChange={() => console.log("slide change")}
               onSwiper={(swiper) => console.log(swiper)}
+              style={{
+                height: "235px",
+              }}
             >
               <SwiperSlide>
                 <SmallCard />
